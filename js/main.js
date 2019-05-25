98a9fe5d3f96749f28c951af0d49b54f3e0dba13
kkentauro <kkentauroaigis@gmail.com>
2019-05-25 02:43:15 -0400

    multiple file loading, ui improvements

---
+++ js/main.js
@@ -0,0 +1,116 @@
+/**
+library.js
+
+tools for translating
+**/
+
+const ascii_to_utf8 = function(str) {
+	return decodeURIComponent(escape(str));
+};
+
+const utf8_to_ascii = function(str) {
+	return unescape(encodeURIComponent(str));
+};
+
+const [load_files, load_zip] = (function() {
+	function confirm_overwrite() {
+		if(vue.files.length > 0) {
+			if(!confirm("This will overwrite duplicate files. Continue?")) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	function load_files() {
+		let upload_form = $("#upload")[0];
+		
+		if(!upload_form.files) { return; }
+		if(!confirm_overwrite()) { return; }
+		
+		for(let file of upload_form.files) {
+			let fr = new FileReader();
+			
+			fr.onload = function(e) {
+				add_tl_file(file.name, e.target.result);
+				done_loading();
+			}
+			
+			fr.readAsText(file);
+		}
+	}
+	
+	
+	function add_tl_file(filename, b64str) {
+		let text = atob(b64str);
+		let matches = text.matchAll(/(?<jp>[^\0]+)\0(?<en>[^\0]*)\0\0\n/g);
+		let pairs = [];
+		let match = matches.next();
+		
+		while(!match.done) {
+			let jp = ascii_to_utf8(match.value.groups["jp"]);
+			let en = ascii_to_utf8(match.value.groups["en"]);
+			pairs.push({jp: jp, en: en});
+			
+			match = matches.next();
+		}
+		
+		let index = vue.files.findIndex(file => file.name === filename);
+
+		if(index >= 0) {
+			vue.files.splice(index, 1);
+		}
+		
+		vue.files.push({name: filename, pairs: pairs});
+	}
+	
+	function done_loading() {
+		vue.files = vue.files.sort(function(a, b){
+			if(a.name.toLowerCase() < b.name.toLowerCase()) { return -1; }
+			if(a.name.toLowerCase() > b.name.toLowerCase()) { return  1; }
+			return 0;
+		});
+		
+		$("#loading").addClass("hidden");
+	};
+	
+	function parse_zip(zip) {
+		let promises = [];
+		
+		for(let file of Object.values(zip.files)) {
+			let promise = file.async("text")
+				.then(text => add_tl_file(file.name, text))
+				.finally();
+				
+			promises.push(promise);
+		}
+		
+		return Promise.all(promises)
+			.then(done_loading);
+	}
+	
+	function load_zip() {
+		let upload_form = $("#upload")[0];
+		let file = upload_form.files[0];
+		
+		if(!file) { return; }
+		if(!confirm_overwrite()) { return; }
+		
+		$("#zip_loading").removeClass("hidden");
+		
+		JSZip.loadAsync(file)
+			.then(parse_zip)
+	}
+	
+	return [load_files, load_zip];
+})();
+
+
+const save_file = async function(text) {
+	let [file, filename] = await Promise.all(vue.dl_file);
+	console.log(file, filename);
+	saveAs(file, filename);
+}
+
+
---
+++ js/vue-components.js
@@ -0,0 +1,81 @@
+
+Vue.component("tl-file", {
+	props: ["file"],
+	methods: {
+		add_pair: function() {
+			this.file.pairs.push({jp: "", en: ""});
+		},
+	},
+	mounted: function() {
+		$(this.$el).accordion({ collapsible: true, active: false });
+	},
+//	template: "<div><tl-pair v-for='(pair, index) in pairs' v-bind:index='index' v-bind:pair='pair'></tl-pair-row><add-pair v-on:add_pair='add_pair();'></add-pair></div>",
+	template: "<div><h3>{{ file.name }}</h3><div><div v-for='(pair, index) in file.pairs'><tl-pair v-bind:index='index' v-bind:pair='pair'></tl-pair></div><add-pair v-on:add_pair='add_pair();'></add-pair></div></div>",
+});
+
+Vue.component("tl-pair", {
+	props: ["pair", "index"],
+	methods: {
+		change_jp: function(value) {
+			this.pair.jp = value;
+		},
+		change_en: function(value) {
+			this.pair.en = value;
+		},
+	},
+	template: "<div style='clear:both;'><div style='float:left; text-align:center; width:4em;'>{{ index }}</div><div style='float:left;'><tl-textarea @changed='change_jp' v-bind:text='pair.jp'></tl-textarea><tl-textarea @changed='change_en' v-bind:text='pair.en'></tl-textarea></div></div>",
+});
+
+Vue.component("tl-textarea", {
+	props: ["text"],
+ 	computed: {
+		_text: {
+			get() {
+				return this.text;
+			},
+			set(value) {
+				this.$emit("changed", value);
+			},
+		},
+	},
+	template: "<textarea rows='5' cols='32' v-model='_text'>{{ _text }}</textarea>",
+});
+
+Vue.component("add-pair", {
+	template: "<div></div>",
+//	template: "<input type='button' id='button' value='Add pair' v-on:click='$emit(`add_pair`)'></input>",
+});
+
+const vue = new Vue({
+	data: {files: []},
+	methods: {
+		encode_file(file) {
+			let strings = [];
+			
+			for(let pair of file.pairs) {
+				let jp = utf8_to_ascii(pair.jp);
+				let en = utf8_to_ascii(pair.en);
+				strings.push(jp + "\0" + en + "\0\0\n");
+			}
+			
+			return btoa(strings.join(""));
+		},
+	},
+	computed: {
+		dl_file: function() {
+			if(this.files.length == 0) {
+				return;
+			}
+			
+			let zip = new JSZip();
+			
+			for(let file of this.files) {
+				zip.file(file.name, this.encode_file(file));
+			}
+			
+			return [zip.generateAsync({type:"blob"}), "strings.zip"];
+		},
+	},
+	el: "#vue-root",
+	template: "<div id='main'><div v-for='file in files'><tl-file v-bind:file='file'></tl-file></div></div>",
+})
