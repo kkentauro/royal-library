ef90b30ba1668e0a61bce80078e9f0f5ac7f68d5
kkentauro <kkentauroaigis@gmail.com>
2019-05-24 22:04:22 -0400

    save file

--- js/translation.js
+++ js/translation.js
@@ -4,40 +4,71 @@ library.js
 tools for translating
 **/
 
-let global = {};
+const global = {};
 
-ascii_to_utf8 = function(str) {
+const ascii_to_utf8 = function(str) {
 	return decodeURIComponent(escape(str));
 };
 
-utf8_to_ascii = function(str) {
+const utf8_to_ascii = function(str) {
 	return unescape(encodeURIComponent(str));
 };
 
-function parse_file() {
-	let file = $("#upload")[0].files[0];
-	let fr = new FileReader();
-	fr.onload = e => load_file(atob(e.target.result));
-	fr.readAsText(file);
-}
-
-function load_file(text) {
-	let matches = text.matchAll(/(?<jp>[^\0]+)\0(?<en>[^\0]*)\0\0\n/g);
-	let pairs = [];
-	let match = matches.next();
+const parse_file = (function() {
 	
-	while(!match.done) {
-		let jp = ascii_to_utf8(match.value.groups["jp"]);
-		let en = ascii_to_utf8(match.value.groups["en"]);
-		vue.pairs.push({jp: jp, en: en});
+	function load_file(text) {
+		let matches = text.matchAll(/(?<jp>[^\0]+)\0(?<en>[^\0]*)\0\0\n/g);
+		let pairs = [];
+		let match = matches.next();
 		
-		match = matches.next();
+		while(!match.done) {
+			let jp = ascii_to_utf8(match.value.groups["jp"]);
+			let en = ascii_to_utf8(match.value.groups["en"]);
+			vue.pairs.push({jp: jp, en: en});
+			
+			match = matches.next();
+		}
+		
 	}
 	
-}
+	function parse_file() {
+		if(vue.pairs.length > 0) {
+			if(!confirm("This will remove all current entries. Load file?")) {
+				return;
+			} else {
+				vue.pairs = [];
+			}
+		}
+		
+		let upload_form = $("#upload")[0];
+		let file = upload_form.files[0];
+		let fr = new FileReader();
+		
+		if(!file) {
+			return;
+		}
+		
+		global.filename = upload_form.value.split("\\").pop();
+		fr.onload = e => load_file(atob(e.target.result));
+		fr.readAsText(file);
+	}
+	
+	return parse_file;
+})();
+
 
 function save_file(text) {
+	if(vue.pairs.length == 0) {
+		return;
+	}
 	
+	let str = vue.pairs.reduce(function(accum, val) {
+		return accum + utf8_to_ascii(val.jp)
+			+ "\0" + utf8_to_ascii(val.en) + "\0\0\n";
+	}, "");
+	
+	let b64str = btoa(str);
+	saveAs(new Blob([b64str]), global.filename);
 }
 
 
@@ -66,11 +97,11 @@ Vue.component("tl-pairs", {
 			return btoa(strings.join(""));
 		},
 	},
-	template: "<div><div v-for='pair in pairs'><tl-pair-row v-bind:pair='pair'></tl-pair-row></div><add-pair v-on:add_pair='add_pair();'></add-pair></div>",
+	template: "<div><div v-for='(pair, index) in pairs'><tl-pair-row v-bind:index='index' v-bind:pair='pair'></tl-pair-row></div><add-pair v-on:add_pair='add_pair();'></add-pair></div>",
 });
 
 Vue.component("tl-pair-row", {
-	props: ["pair"],
+	props: ["pair", "index"],
 	methods: {
 		change_jp: function(value) {
 			this.pair.jp = value;
@@ -79,7 +110,7 @@ Vue.component("tl-pair-row", {
 			this.pair.en = value;
 		},
 	},
-	template: "<div><tl-textarea @changed='change_jp' v-bind:text='pair.jp'></tl-textarea><tl-textarea @changed='change_en' v-bind:text='pair.en'></tl-textarea></div>",
+	template: "<div style='clear:both;'><div style='float:left; text-align:center; width:4em;'>{{ index }}</div><div style='float:left;'><tl-textarea @changed='change_jp' v-bind:text='pair.jp'></tl-textarea><tl-textarea @changed='change_en' v-bind:text='pair.en'></tl-textarea></div></div>",
 });
 
 Vue.component("tl-textarea", {
@@ -90,22 +121,16 @@ Vue.component("tl-textarea", {
 				return this.text;
 			},
 			set(value) {
-				console.log(this);
 				this.$emit("changed", value);
 			},
 		},
 	},
-	template: "<textarea v-model='_text'>{{ _text }}</textarea>",
+	template: "<textarea rows='5' cols='32' v-model='_text'>{{ _text }}</textarea>",
 });
 
 Vue.component("add-pair", {
-	methods: {
-		add_pair: function() {
-			console.log(this);
-			this.$emit("add_pair");
-		}
-	},
-	template: "<input type='button' id='button' value='Add pair' v-on:click='add_pair();'></input>",
+	template: "",
+//	template: "<input type='button' id='button' value='Add pair' v-on:click='$emit(`add_pair`)'></input>",
 });
 
 const vue = new Vue({
